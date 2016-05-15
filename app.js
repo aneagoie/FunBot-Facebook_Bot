224971c282a0e80ef79b74c6aac9b9d5e5b0a4c4
Andrei <aneagoie1@gmail.com>
2016-05-15 11:35:20 -0400

    added webhook

--- app.js
+++ app.js
@@ -13,22 +13,51 @@ app.get('/', function (req, res) {
   res.send('Hello World! I am the FunBot!');
 });
 
-app.get('/webhook', function (req, res) {
-  if (req.query['hub.verify_token'] === 'funbottoken') {
-    res.send(req.query['hub.challenge']);
-  } else {
-    res.send('Error, wrong validation token');
-  }
-});
+// app.get('/webhook', function (req, res) {
+//   if (req.query['hub.verify_token'] === 'funbottoken') {
+//     res.send(req.query['hub.challenge']);
+//   } else {
+//     res.send('Error, wrong validation token');
+//   }
+// });
 
 app.post('/webhook', function (req, res) {
-  if (req.query['hub.verify_token'] === 'funbottoken') {
-    res.send(req.query['hub.challenge']);
-  } else {
-    res.send('Error, wrong validation token');
-  }
+   messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
 });
 
-app.listen(process.env.PORT, function () {
+var token = 'EAAIbFiOeJBcBAERNppvdEyMBFkyeLc5475SRnM7HOztLNc92VBsnvhQmFHIgQGxmQMgunpeyMQ7jOI2CPjrPsO2zoSg7Y2DZBnp6jvHsBPNMmm4DvyJZBBnYBSgnO6qumq1EHbZAZBiZBjqrBItjrYbyZAPbrHEqBUQHGLrxA9NwZDZD';
+
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
+
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
