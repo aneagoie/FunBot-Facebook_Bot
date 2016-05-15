3bd09d514cf065207f0dc3f391c34cef3f4c19a9
Andrei <aneagoie1@gmail.com>
2016-05-15 12:56:04 -0400

    added hack to fix known bug

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # FunBot-Facebook_Bot
 Facebook Bot that tells you jokes
+A fun little project learning how to make a facebook bot.
+
+This bot uses Modulus as the app container.
--- app.js
+++ app.js
@@ -2,7 +2,57 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
 var app = express();
+var JSONbig = require('json-bigint');
 var token = 'EAAIbFiOeJBcBAERNppvdEyMBFkyeLc5475SRnM7HOztLNc92VBsnvhQmFHIgQGxmQMgunpeyMQ7jOI2CPjrPsO2zoSg7Y2DZBnp6jvHsBPNMmm4DvyJZBBnYBSgnO6qumq1EHbZAZBiZBjqrBItjrYbyZAPbrHEqBUQHGLrxA9NwZDZD';
+
+function sendGenericMessage(sender) {
+    messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "First card",
+                    "subtitle": "Element #1 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "https://www.messenger.com",
+                        "title": "web url"
+                    }, {
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for first element in a generic bubble",
+                    }],
+                }, {
+                    "title": "Second card",
+                    "subtitle": "Element #2 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+                    "buttons": [{
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for second element in a generic bubble",
+                    }],
+                }]
+            }
+        }
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
 
 function sendTextMessage(sender, text) {
   messageData = {
@@ -25,29 +75,56 @@ function sendTextMessage(sender, text) {
   })
 }
 
-// Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+//Temporary until facebook fixes their parsing issue.
+// // Process application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({extended: false}))
 
-// Process application/json
-app.use(bodyParser.json())
+// // Process application/json
+// app.use(bodyParser.json())
+
+//Hack from https://developers.facebook.com/bugs/578746852290927/
+app.use(function(req, res, next){
+  if (req.method == 'POST') {
+    var body = '';
+
+    req.on('data', function (data) {
+      body += data;
+
+      // Too much POST data, kill the connection!
+      // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+      if (body.length > 1e6)
+        req.connection.destroy();
+    });
+
+    req.on('end', function () {
+      // console.log(body); // should work
+        // use post['blah'], etc.
+      req.body = JSONbig.parse(body)
+      next()
+    });
+  }
+})
 
 app.get('/', function (req, res) {
   res.send('Hello World! I am the FunBot!');
 });
 
-app.post('/webhook', function (req, res) {
-  messaging_events = req.body.entry[0].messaging
-  for (i = 0; i < messaging_events.length; i++) {
-    event = req.body.entry[0].messaging[i]
-    sender = event.sender.id
-    if (event.message && event.message.text) {
-      text = event.message.text
-      console.log(text);
-      sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id.toString()
+        if (event.message && event.message.text) {
+            text = event.message.text
+            if (text === 'Generic') {
+                sendGenericMessage(sender)
+                continue
+            }
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
     }
-  }
-  res.sendStatus(200)
-});
+    res.sendStatus(200)
+})
 
 
 app.listen(process.env.PORT || 3000, function () {
